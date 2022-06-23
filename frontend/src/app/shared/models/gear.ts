45d4e425b7412c98e60758f8a991e572b83ed6f7
Zaiddc <zaid.dc7@gmail.com>
2022-06-23 10:35:44 +0300

    Modify#1

--- frontend/src/app/shared/models/gear.ts
+++ frontend/src/app/shared/models/gear.ts
@@ -1,7 +1,7 @@
-export class Gear{
-  id!:string;
+export class Gear{  /* A class is created with its members*/ 
+  id!:string;             /* ! indicates that the member/field is required */ 
   name!:string;
-  price!:number;
+  price!:number;          /* ? indicates that the member/field is optional */ 
   tags?: string[];
   favorite!:boolean;
   stars!: number;
